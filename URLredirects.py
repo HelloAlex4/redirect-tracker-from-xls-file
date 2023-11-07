4d1179d9be4aa2791e4e8e4bc78b204df1be4e26
HelloAlex4 <106261202+HelloAlex4@users.noreply.github.com>
2023-11-07 18:46:48 +0100

    Add files via upload

---
+++ URLredirects.py
@@ -0,0 +1,25 @@
+import requests
+from openpyxl import load_workbook
+
+#file = ("Example URLs Tescases.xlsx")
+file = input("Enter Path of the Excel File (including file extention): ")
+
+workbook = load_workbook(file)
+
+sheet = workbook.active
+
+
+for x in range(sheet.max_row - 1):
+	toBeRedirectedURL = (sheet.cell(row = x + 2, column = 1).value)
+
+
+	r = requests.get(toBeRedirectedURL)
+	redirectedURL = r.url
+	strippedURL = redirectedURL.split("?", 1)[0]
+
+	cell = str("B" + str(x + 2))
+	sheet[cell] = strippedURL
+	print(strippedURL)
+
+workbook.save(file)
+print("FINISHED")
